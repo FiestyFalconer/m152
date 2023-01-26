b989670ecd9765a7b925dc6156ccbb84e9ef45a9
Rodrigo <60130986+FiestyFalconer@users.noreply.github.com>
2023-01-26 15:13:02 +0100

    chapitre_1 terminée

---
+++ chapitre_1/post.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link rel="stylesheet" href="./style/stylesheet.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+<header class="py-3 mb-2 bg-primary textwhite">
+        <ul class="nav nav-pills d-flex justify-content-between">
+            <li calss="nav-item">
+                <i id="logo" class="fab fa-bootstrap"></i>
+            <li calss="nav-item">
+                <form class="navbar-form navbar-left">
+                    <div class="input-group input-group-sm">
+                        <input id="search" class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+                        <div class="input-group-btn">
+                            <button class="btn btn-light" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a href="./home.php" class="fa-solid fa-house nav-link active" aria-current="page"> Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="./post.php" class="fa-solid fa-plus nav-link active" aria-current="page"> Post</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="fa-solid fa-user fa-solid fa-plus nav-link active" aria-current="page"></a>
+            </li>
+        </ul>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
